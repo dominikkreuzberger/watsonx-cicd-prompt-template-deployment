5fc3dd9c2f41f8839ccd5675c94cd039e896651c
Dominik Kreuzberger <Dominik.Kreuzberger@ibm.com>
2025-11-19 17:43:11 +0100

    add
    add

--- python-scripts/python-script-for-watsonx-prompt-deployment.py
+++ python-scripts/python-script-for-watsonx-prompt-deployment.py
@@ -30,7 +30,6 @@ prompt_template = PromptTemplate(name="New prompt from cicd",
                                             "A loan is a debt that is repaid with interest over time."]]
                                 )
 
-
 stored_prompt_template = prompt_mgr.store_prompt(prompt_template=prompt_template)
 
 from ibm_watsonx_ai import APIClient
@@ -41,12 +40,11 @@ client.set.default_project(project_id)
 prompt_input_text = prompt_mgr.load_prompt(prompt_id=stored_prompt_template.prompt_id, 
                                            astype=PromptTemplateFormats.STRING)
 
+task_credential = client.task_credentials.store("wx task credentials")
+
 meta_props = {
     client.deployments.ConfigurationMetaNames.NAME: "SAMPLE DEPLOYMENT PROMPT TEMPLATE",
     client.deployments.ConfigurationMetaNames.ONLINE: {},
-    client.deployments.ConfigurationMetaNames.TASK: {
-        "api_key": os.getenv("WATSONX_TASK_CREDENTIAL")
-    },
     client.deployments.ConfigurationMetaNames.BASE_MODEL_ID: "ibm/granite-3-8b-instruct"}
 
 deployment_details = client.deployments.create(artifact_id=stored_prompt_template.prompt_id, meta_props=meta_props)
