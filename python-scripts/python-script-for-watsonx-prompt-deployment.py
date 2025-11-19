9ea504153d469f16db2e4714f28064c20fcb66c7
Dominik Kreuzberger <Dominik.Kreuzberger@ibm.com>
2025-11-19 16:41:29 +0100

    update
    update

--- python-scripts/python-script-for-watsonx-prompt-deployment.py
+++ python-scripts/python-script-for-watsonx-prompt-deployment.py
@@ -7,7 +7,6 @@ watsonx_apikey = os.getenv("WATSONX_API_KEY")
 watsonx_url = os.getenv("WATSONX_URL")
 space_id = os.getenv("WATSONX_SPACE_ID")
 project_id = os.getenv("WATSONX_PROJECT_ID")
-project_id = space_id
 credentials = {
     "apikey": watsonx_apikey,
     "url": watsonx_url
